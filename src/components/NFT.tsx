70cc4d1b9f6be63ad08f422f7d23e16fa0f82e8a
sandie69p <salvatore.pisu04@gmail.com>
2026-05-14 11:49:36 +0200

    feat: adding contract to portfolio

---
+++ src/components/NFT.tsx
@@ -0,0 +1,13 @@
+function NFT() {
+    return (
+        <div className="nft">
+            <p>Nome: /*Nome*/</p>
+            <address>Indirizzo: /*indirizzo*/</address>
+            <p>Sigla: /*sigla*/</p>
+            <p>Prezzo: /*prezzo*/</p>
+            <p>Owner: <address>/*owner*/</address></p>
+        </div>
+    );
+}
+
+export default NFT;
